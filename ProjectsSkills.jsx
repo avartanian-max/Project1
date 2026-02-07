6d7c1c80ea6087809ec54ea8587bf66bdd58bfda
jwilliams <jwilliams@odin.cs.csubak.edu>
2026-02-06 19:41:27 -0800

    Joshua's technical skills and project ideas added.

--- ProjectsSkills.jsx
+++ ProjectsSkills.jsx
@@ -4,6 +4,9 @@ const ProjectsSkills = () => {
   return (
     <View style ={styles.container}>
       <Text style={styles.title}>Projects / Skills</Text>
+      <Text> Joshua's Technical Skills: Windows Troubleshooting, Hardware Troubleshooting, Coding </Text>
+      <Text> It would be cool to try doing something more complicated for the next project.
+             Creating a game or doing something more technical is what I'm looking for. </Text>
     </View>
   )
 }
