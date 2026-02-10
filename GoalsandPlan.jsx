77b3646a79610eb64fdad2ca9e9b95b80e7f8794
jwilliams <jwilliams@odin.cs.csubak.edu>
2026-02-09 17:32:57 -0800

    Joshua's Goals added

--- GoalsandPlan.jsx
+++ GoalsandPlan.jsx
@@ -5,7 +5,17 @@ const GoalsandPlan = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Goals & Plan</Text>
+
+    <View style={styles.card}>
+    <Text> Joshua's Learning Goals: </Text>
+    <Text> Learn how to create an app. </Text>
+    <Text> Learn how to solve problems with embedded systems. </Text>
+    <Text> Learn how to use AI to it's best ability. </Text>
+    <Text> </Text>
+    <Text> I believe these goals matter because they should be vital to the other
+                 classes we will be doing in the future. </Text>
     </View>
+   </View> 
   )
 }
 
