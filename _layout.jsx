1bce8231b965b81333d58f2eb5c53fc68bd28c53
avartanian-max <avartanian@csub.edu>
2026-02-11 01:51:01 -0800

    Create layout page

---
+++ _layout.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, useColorScheme, View } from 'react-native'
+import { Stack } from 'expo-router'
+import { Colors } from "../constants/Colors"
+
+const Rootlayout = () => {
+    const colorScheme = useColorScheme()
+    const theme = Colors[colorScheme] ?? Colors.light
+  return (
+    <>
+        <Stack screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+
+        }}>
+            <Stack.Screen name="(dashboard)" options={{title: ''}} />
+            <Stack.Screen name="index" options={{ title: 'Home'}} />
+            <Stack.Screen name="about" options={{ title: 'About'}} />
+            <Stack.Screen name="contact" options={{ title: 'Contact'}} />
+        </Stack>
+        </>
+        
+  )
+}
+
+
+export default Rootlayout
+
+const styles = StyleSheet.create({})
