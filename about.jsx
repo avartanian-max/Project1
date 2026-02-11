1587b39daf40ae372ace01777dc4d55ef5f324a2
avartanian-max <avartanian@csub.edu>
2026-02-11 01:37:29 -0800

    Create about page

---
+++ about.jsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import ThemedView from '../components/ThemedView'
+import ThemedLogo from '../components/ThemedLogo'
+import Spacer from '../components/Spacer'
+import ThemedText from '../components/ThemedText'
+
+const About = () => {
+
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.title}>About Page</ThemedText>
+
+      <Link href ="/"style={styles.link}>
+      <ThemedText>Home</ThemedText>
+      </Link>
+    </ThemedView>
+  )
+}
+
+export default About
+
+const styles = StyleSheet.create({container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    link: {
+        marginVertical: 10,
+        borderBottomWidth: 1
+    }
+})
