17b94ad1b4a4ccfbdc0d92fcc9100d4f4ebf495a
jwilliams140 <jwilliams140@csub.edu>
2026-02-11 18:52:07 -0800

    Galleries

---
+++ Gallery.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Gallery</Text>
+    </View>
+  )
+}
+
+export default Gallery
+
+const styles = StyleSheet.create({})
