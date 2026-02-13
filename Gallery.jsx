8f72e58d7f5f20112abee5e62cf71cb341773ad3
jwilliams <jwilliams@odin.cs.csubak.edu>
2026-02-12 17:28:01 -0800

    Card added to Gallery

--- Gallery.jsx
+++ Gallery.jsx
@@ -5,6 +5,10 @@ const Gallery = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Gallery</Text>
+
+      <View style={styles.card} >
+        <Text> </Text>
+        </View>
     </View>
   )
 }
@@ -17,8 +21,19 @@ const styles = StyleSheet.create({
     alignItems: 'center' ,
     justifyContent: 'center'
   },
+
   title: {
     fontWeight: 'bold' ,
     fontSize: 25,
   },
+
+  card: {
+    backgroundColor: '#ADD8E6',
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 5,
+    width: 400,
+    height: 200,
+    boxShadow: '4px 4px rgba(0,0,0,0.1)'
+  }
 })
