49c6a3e64af01f08f2cdf00824c59e174d043179
jwilliams <jwilliams@odin.cs.csubak.edu>
2026-02-14 17:51:39 -0800

    Alex's logos added. Cards reduced and changed for simplicity

--- Gallery.jsx
+++ Gallery.jsx
@@ -4,6 +4,7 @@ import December2025 from '../assets/img/December2025.jpg'
 import ACertification from '../assets/img/A+Certification.jpg'
 import NetworkPlus from '../assets/img/Network-1.jpg'  
 import DarkLogo from '../assets/img/csubdark.jpg' 
+import LightLogo from '../assets/img/csublight.jpg'  
 
 const Gallery = () => {
   return (
@@ -12,17 +13,32 @@ const Gallery = () => {
       <Image source={ACertification} />
       <Image source={NetworkPlus} />
       <Image source={DarkLogo} />
+      <Image source={LightLogo} />
 
       <View style={styles.topRightElement} >
         <Text> My siblings & I supporting Sable Bourbon </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> A+ Certification from Junior Year 2018 </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>
+        <Text> </Text>      
+        <Text> Network+ Certification from Senior Year 2020 </Text>
         </View>
 
-      <View style={styles.topCenterElement} >
-        <Text> A+ Certification from Junior Year 2019 </Text>
-        </View>
-
-      <View style={styles.topBottomElement} >
-      <Text> Network+ Certification from Senior Year 2020 </Text>
+      <View style={styles.topMiddleElement} >
+      <Text> CSUB Dark Logo </Text>
+      <Text> </Text>
+      <Text> </Text>
+      <Text> </Text>
+      <Text> </Text>
+      <Text> </Text>
+      <Text> CSUB Light Logo</Text>
       </View>
 
     </View>
@@ -52,37 +68,25 @@ const styles = StyleSheet.create({
    topRightElement: {
     // The child element is positioned relative to its parent container
      position: 'absolute',
-     top: 2,  // Adjust distance from the top
-     right: 10, // Adjust distance from the right
+     top: 0,  // Adjust distance from the top
+     right: 0, // Adjust distance from the right
      backgroundColor: 'lightblue',
      padding: 5,
      borderRadius: 5,
-     height: 150,
-     width: 200,
+     height: 378,
+     width: 253,
   },
 
-    topCenterElement: {
+    topMiddleElement: {
     // The child element is positioned relative to its parent container
     position: 'absolute',
-    top: 155,  // Adjust distance from the top
-    right: 10, // Adjust distance from the right
+    top: 378,  // Adjust distance from the top
+    right: 0, // Adjust distance from the right
     backgroundColor: 'lightblue',
     padding: 5,
     borderRadius: 5,
-    height: 130,
-    width: 200,
-  },
-
-   topBottomElement: {
-    // The child element is positioned relative to its parent container
-    position: 'absolute',
-    top: 310,  // Adjust distance from the top
-    right: 10, // Adjust distance from the right
-    backgroundColor: 'lightblue',
-    padding: 5,
-    borderRadius: 5,
-    height: 130,
-    width: 200,
+    height: 378,
+    width: 253,
   },
 })
 
