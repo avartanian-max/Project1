fa52b0c3059347c2afc42063125c5c2ebf027762
avartanian-max <avartanian@csub.edu>
2026-02-15 22:57:11 -0800

    Update _layout.jsx

--- _layout.jsx
+++ _layout.jsx
@@ -12,10 +12,12 @@ const Rootlayout = () => {
             headerTintColor: theme.title,
 
         }}>
-            <Stack.Screen name="(dashboard)" options={{title: ''}} />
+            
             <Stack.Screen name="index" options={{ title: 'Home'}} />
-            <Stack.Screen name="about" options={{ title: 'About'}} />
-            <Stack.Screen name="contact" options={{ title: 'Contact'}} />
+            <Stack.Screen name="about" options={{ title: 'Student Info'}} />
+            <Stack.Screen name="appinfo" options={{ title: 'App Info'}} />
+            <Stack.Screen name="ProjectsSkills" options={{ title: 'Projects & Skills'}} />
+            <Stack.Screen name="GoalsandPlan" options={{ title: 'Goals & Plans'}} />
         </Stack>
         </>
         
