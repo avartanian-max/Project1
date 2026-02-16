64d97b5c8a90b6c8f94c95b542d7ebee2412b3f6
avartanian-max <avartanian@csub.edu>
2026-02-15 22:56:55 -0800

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -16,22 +16,31 @@ const Home = () => {
       <ThemedText style ={styles.title} title={true}
       >Student Introductions
       </ThemedText>
-    <Spacer height ={10}/>
-      <ThemedText> By: Joshua Williams, Alexander Vartanian </ThemedText>
+    <Spacer height ={10}/> 
+      <ThemedText style = {styles.text}> By: Joshua Williams, Alexander Vartanian </ThemedText>
     <Spacer height = {20}/>
 
     <View style = {styles.card}>
-        <Text></Text>
+        <ThemedText style = {styles.text}>                  Welcome to our app</ThemedText>
+        <ThemedText style = {styles.text}>       This app serves as an introduction</ThemedText>
+        <ThemedText style = {styles.text}>to display our current skills and future goals</ThemedText>
     </View>
     <Link href="/about" style={styles.link}>
-    <ThemedText>About Page</ThemedText>
-    </Link>
-    <Link href="/contact" style={styles.link}>
     <ThemedText>Student Info</ThemedText>
     </Link>
-    <Link href="/profile" style={styles.link}>
-    <ThemedText>Placeholder</ThemedText>
+    <Link href="/appinfo" style={styles.link}>
+    <ThemedText>About the App</ThemedText>
     </Link>
+    <Link href="/ProjectsSkills" style={styles.link}>
+    <ThemedText>Projects & Skills</ThemedText>
+    </Link>
+    <Link href="/GoalsandPlan" style={styles.link}>
+    <ThemedText>Goals & Plans</ThemedText>
+    </Link>
+    <Link href="/Gallery" style={styles.link}>
+    <ThemedText>Gallery</ThemedText>
+    </Link>
+    
     </ThemedView>
   )
 }
@@ -49,7 +58,7 @@ const styles = StyleSheet.create({
         fontSize: 18
     },
     card: {
-        backgroundColor: '#eee',
+        backgroundColor: '#ADD8E6',
         padding: 20,
         borderRadius: 5,
         boxShadow: '4px 4px rgba(0,0,0,0.1)'
